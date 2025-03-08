4fcc3101531246ac09bb32427f6c5a961d306ea8
Ros McMahon <rosmcmahon@gmail.com>
2025-03-09 01:38:31 +0700

    quick-tap-term => 0 on spacebar

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
@@ -14,64 +14,83 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
+ #include QMK_KEYBOARD_H
 
-enum layers {
-    MAC_BASE,
-    WIN_BASE,
-    MAC_FN,
-    WIN_FN,
-    L_FN1,
-};
-
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_61_ansi(
-        KC_GRV,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,   KC_EQL,  KC_ESC,
-        KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,   KC_RBRC, KC_BSLS,
-        KC_CAPS,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
-        KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LOPTN,     KC_LCMMD,                           LT(4, KC_SPC),                                                  KC_RCMMD,   MO(MAC_FN), MO(L_FN1),  KC_RCTL),
-
-    [WIN_BASE] = LAYOUT_61_ansi(
-        KC_ESC,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,  KC_RBRC,  KC_BSLS,
-        KC_CAPS,  KC_A,         KC_S,           KC_D,           KC_F,           KC_G,     KC_H,     KC_J,           KC_K,           KC_L,           KC_SCLN,            KC_QUOT,            KC_ENT,
-        KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,      KC_LALT,                            LT(4, KC_SPC),                                                  KC_RALT,    MO(WIN_FN), MO(L_FN1),  KC_RCTL),
-
-    [MAC_FN] = LAYOUT_61_ansi(
-        KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,   KC_PGUP,  KC_HOME,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  KC_UP,    KC_SNAP,  KC_PGDN,  KC_END,             _______,
-        _______,            RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_DEL,             _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
-
-    [WIN_FN] = LAYOUT_61_ansi(
-        KC_GRV,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  KC_APP,   KC_SCRL,  KC_INS,   KC_PGUP,  KC_HOME,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  KC_UP,    KC_PSCR,  KC_PGDN,  KC_END,             _______,
-        _______,            RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_DEL,             _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
-
-    [L_FN1] = LAYOUT_61_ansi(
-        _______,  KC_F1,        KC_F2,          KC_F3,          KC_F4,          KC_F5,     KC_F6,   KC_F7,          KC_F8,          KC_F9,          KC_F10,             KC_F11,   KC_F12,   _______,
-        _______,  _______,      KC_MS_BTN1,     _______,        _______,        _______,   KC_INS,  KC_HOME,        KC_UP,          KC_END,         KC_MINS,            KC_EQL,   _______,  _______,
-        _______,  _______,      _______,        _______,        _______,        _______,   KC_PGUP, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_ESC,             _______,            _______,
-        _______,                _______,        _______,        _______,        _______,   BAT_LVL, KC_PGDN,        KC_DEL,         KC_BSPC,        _______,            KC_BSLS,            _______,
-        _______,  _______,      _______,                                                  _______,                                                  _______,  _______,  _______,  _______),
-};
-
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LGUI_T(KC_D):
-        case RGUI_T(KC_K):
-        case LSFT_T(KC_F):
-        case RSFT_T(KC_J):
-        case LT(4, KC_SPC):
-            return true;
-        default:
-            // Do not select the hold action when another key is tapped.
-            return false;
-    }
-}
+ enum layers {
+     MAC_BASE,
+     WIN_BASE,
+     MAC_FN,
+     WIN_FN,
+     L_FN1,
+ };
+ 
+ // clang-format off
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     [MAC_BASE] = LAYOUT_61_ansi(
+         KC_GRV,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,   KC_EQL,  KC_ESC,
+         KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,   KC_RBRC, KC_BSLS,
+         KC_CAPS,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
+         KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
+         KC_LCTL,  KC_LOPTN,     KC_LCMMD,                           LT(4, KC_SPC),                                                  KC_RCMMD,   MO(MAC_FN), MO(L_FN1),  KC_RCTL),
+ 
+     [WIN_BASE] = LAYOUT_61_ansi(
+         KC_ESC,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,  KC_EQL,   KC_BSPC,
+         KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,  KC_RBRC,  KC_BSLS,
+         KC_CAPS,  KC_A,         KC_S,           KC_D,           KC_F,           KC_G,     KC_H,     KC_J,           KC_K,           KC_L,           KC_SCLN,            KC_QUOT,            KC_ENT,
+         KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
+         KC_LCTL,  KC_LWIN,      KC_LALT,                            LT(4, KC_SPC),                                                  KC_RALT,    MO(WIN_FN), MO(L_FN1),  KC_RCTL),
+ 
+     [MAC_FN] = LAYOUT_61_ansi(
+         KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,
+         _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,   KC_PGUP,  KC_HOME,  _______,
+         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  KC_UP,    KC_SNAP,  KC_PGDN,  KC_END,             _______,
+         _______,            RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_DEL,             _______,
+         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+ 
+     [WIN_FN] = LAYOUT_61_ansi(
+         KC_GRV,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,
+         _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  KC_APP,   KC_SCRL,  KC_INS,   KC_PGUP,  KC_HOME,  _______,
+         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  KC_UP,    KC_PSCR,  KC_PGDN,  KC_END,             _______,
+         _______,            RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_DEL,             _______,
+         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+ 
+     [L_FN1] = LAYOUT_61_ansi(
+         _______,  KC_F1,        KC_F2,          KC_F3,          KC_F4,          KC_F5,     KC_F6,   KC_F7,          KC_F8,          KC_F9,          KC_F10,             KC_F11,   KC_F12,   _______,
+         _______,  _______,      KC_MS_BTN1,     _______,        _______,        _______,   KC_INS,  KC_HOME,        KC_UP,          KC_END,         KC_MINS,            KC_EQL,   _______,  _______,
+         _______,  _______,      _______,        _______,        _______,        _______,   KC_PGUP, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_ESC,             _______,            _______,
+         _______,                _______,        _______,        _______,        _______,   BAT_LVL, KC_PGDN,        KC_DEL,         KC_BSPC,        _______,            KC_BSLS,            _______,
+         _______,  _______,      _______,                                                  _______,                                                  _______,  _______,  _______,  _______),
+ };
+ 
+ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+     switch (keycode) {
+         case LGUI_T(KC_D):
+         case RGUI_T(KC_K):
+         case LSFT_T(KC_F):
+         case RSFT_T(KC_J):
+         case LT(4, KC_SPC):
+             return true;
+         default:
+             // Do not select the hold action when another key is tapped.
+             return false;
+     }
+ }
+ 
+ // uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+ //     switch (keycode) {
+ //         case LT(4, KC_SPC):
+ //             return 200;
+ //         default:
+ //             return TAPPING_TERM;
+ //     }
+ // } 
+ 
+ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+     switch (keycode) {
+         case LT(4, KC_SPC):
+             return 0;
+         default:
+             return QUICK_TAP_TERM;
+     }
+ }
+ 
