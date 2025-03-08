6ade76d8dc810f4a66f79a60a3c14bbdfc6b717a
Ros McMahon <rosmcmahon@gmail.com>
2025-03-09 01:32:18 +0700

    base keymaps: missing big backspace => ESC

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
@@ -27,7 +27,7 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_61_ansi(
-        KC_ESC,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,   KC_EQL,  KC_BSPC,
+        KC_GRV,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,   KC_EQL,  KC_ESC,
         KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,   KC_RBRC, KC_BSLS,
         KC_CAPS,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
         KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
