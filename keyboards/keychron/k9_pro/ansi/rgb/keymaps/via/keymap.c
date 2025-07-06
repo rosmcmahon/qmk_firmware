35a52490a704a4cd2c189ecbae7d59a9dc70ad94
Ros McMahon <rosmcmahon@gmail.com>
2025-07-06 15:25:58 +0700

    feat: capslock => grave & spaceFn capsloc

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_61_ansi(
         KC_GRV,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,   KC_EQL,  KC_ESC,
         KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,   KC_RBRC, KC_BSLS,
-        KC_CAPS,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
+        KC_GRV,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
         KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LOPTN,     KC_LCMMD,                           LT(4, KC_SPC),                                                  KC_RCMMD,   MO(MAC_FN), MO(L_FN1),  KC_RCTL),
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_FN1] = LAYOUT_61_ansi(
         _______,  KC_F1,        KC_F2,          KC_F3,          KC_F4,          KC_F5,     KC_F6,   KC_F7,          KC_F8,          KC_F9,          KC_F10,             KC_F11,   KC_F12,   _______,
         _______,  _______,      KC_MS_BTN1,     _______,        _______,        _______,   KC_INS,  KC_HOME,        KC_UP,          KC_END,         KC_MINS,            KC_EQL,   _______,  _______,
-        _______,  _______,      _______,        _______,        _______,        _______,   KC_PGUP, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_ESC,             _______,            _______,
+        KC_CAPS,  _______,      _______,        _______,        _______,        _______,   KC_PGUP, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_ESC,             _______,            _______,
         _______,                _______,        _______,        _______,        _______,   BAT_LVL, KC_PGDN,        KC_DEL,         KC_BSPC,        _______,            KC_BSLS,            _______,
         _______,  _______,      _______,                                                  _______,                                                  _______,  _______,  _______,  _______),
 };
@@ -76,7 +76,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-
+// !!! new glitch: d => a sometimes produces cmd + a !!!
 
 // uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 //     switch (keycode) {
