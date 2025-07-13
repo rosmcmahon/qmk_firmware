73b0c685675c6522771144007253f2125821de31
Ros McMahon <rosmcmahon@gmail.com>
2025-07-13 15:07:16 +0700

    feat: use LCMD for new NUMS layer (might be temporary)

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,     KC_Y,     KC_U,           KC_I,           KC_O,           KC_P,               KC_LBRC,   KC_RBRC, KC_BSLS,
         KC_GRV,  LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,     KC_H,     RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN),    KC_QUOT,            KC_ENT,
         KC_LSFT,                KC_Z,           KC_X,           KC_C,           KC_V,     KC_B,     KC_N,           KC_M,           KC_COMM,        KC_DOT,             KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LOPTN,     KC_LCMMD,                           LT(4, KC_SPC),                                                  KC_RCMMD,   MO(MAC_FN), MO(L_FN1),  KC_RCTL),
+        KC_LCTL,  KC_LOPTN,     MO(L_NUMS),                         LT(4, KC_SPC),                                                  KC_RCMMD,   MO(MAC_FN), MO(L_FN1),  KC_RCTL),
 
     [WIN_BASE] = LAYOUT_61_ansi(
         KC_ESC,   KC_1,         KC_2,           KC_3,           KC_4,           KC_5,     KC_6,     KC_7,           KC_8,           KC_9,           KC_0,               KC_MINS,  KC_EQL,   KC_BSPC,
