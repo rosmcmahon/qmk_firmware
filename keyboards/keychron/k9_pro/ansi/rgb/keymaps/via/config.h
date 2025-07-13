89929a4c8745028a85a18ae0a0fa82163e966901
Ros McMahon <rosmcmahon@gmail.com>
2025-07-14 00:00:41 +0700

    task: comment in config.h about DYNAMIC_KEYMAP_LAYER_COUNT

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/config.h
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/config.h
@@ -5,3 +5,8 @@
 // #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM_PER_KEY
 
+/* cannot put this here, conflicts with ../../../info.json 
+ * #define DYNAMIC_KEYMAP_LAYER_COUNT 6
+ * it should not conflict and take precedence apparently, 
+ * but as workaround we feed the definition in in rules.mk 
+ */
